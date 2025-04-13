48f5c3112c9cbf0b69d4dd740b9a42762a1e66cf
pvlzh <pvl.guzh@outlook.com>
2025-04-13 14:21:23 +0300

    Todos.

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,8 @@ mod systems;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     runtime::run()
 }
+
+// todo: start game screen and selecting start game speed
+// todo: up game speed based on game score
+// todo: restart game from end gamescreen
+// todo: error handlings (removing unwraps)
